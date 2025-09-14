7a7088e45d13294f2c8e8825a06d56607c6afc97
daksh <akshd@outlook.com>
2025-09-14 17:40:28 +0530

    feat: basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Ok;
+use crossterm::event::{self, Event, KeyCode};
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use rodio::{Decoder, OutputStream, OutputStreamBuilder, Sink, stream};
+
+fn main() -> anyhow::Result<()> {
+    println!("🎶 termbeat 🎶");
+    println!("Controls: A=Kick | S=Snare | D=Hi-Hat | Q=Quit");
+    enable_raw_mode()?;
+    loop {
+        if event::poll(std::time::Duration::from_millis(500))? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Char('a') => println!("Kick"),
+                    KeyCode::Char('s') => println!("Snare"),
+                    KeyCode::Char('d') => println!("Hi-Hat"),
+                    KeyCode::Char('q') => break,
+                    _ => (),
+                }
+            }
+        }
+    }
+    disable_raw_mode()?;
+    Ok(())
 }
